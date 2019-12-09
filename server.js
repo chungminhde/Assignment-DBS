12d59b12d1e45598197e8c897e87d5dc8272d65f
builong1999 <55575791+builong1999@users.noreply.github.com>
2019-12-09 21:20:29 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const expressSession = require('express-session');
 const LocalStrategy = require('passport-local').Strategy;
 var sql = require('mssql');
+// install library ejs << npm install ejs >>>
 const keys = require('./mod/key')
 
 // ----------------------------------------------CONFIGURATION------------------------------------
